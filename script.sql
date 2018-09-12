8aae37308a08c59f9f5f87fedf580e96985ae63e
ll9 <llwierer@web.de>
2018-09-12 22:04:19 +0200

    Comments

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
-create table test (id, geometry UNIQUE);
+create table test (id, geometry);
 select RecoverGeometryColumn('test','geometry',4326,'POINT');
 
 insert into test values (1,ST_GeomFromText('POINT (0 0)',4326));
@@ -7,3 +7,5 @@ insert into test values (3,ST_GeomFromText('POINT (0 1)',4326));
 
 update test set geometry=ST_GeomFromText('POINT (2 4)',4326);
 
+-- Zuerst spatalite exe starten, dann '.read "script.sql" utf-8' auführen
+-- Folgendes funktioniert momentan seltsamerweise nicht: spatalite.exe script.sql
